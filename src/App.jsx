7a232ef8fac479e1c7ee90f6897300310c74c92c
MaRuF2131 <marufahmmed916@gmail.com>
2025-08-16 13:29:25 +0600

    Added ScrollToTop component for smooth scroll on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,12 +8,31 @@ import About from './pages/About';
 import './App.css';
 import Features from './components/Features';
 import Community from './pages/Community';
+import { useEffect } from 'react';
+
+  // when i change the url, i want to scroll to the top smoothly
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
+
   return (
     <Router>
       <div className="App">
         <Header />
+       {/* ScrollToTop will handle smooth scrolling on route change */}
+        <ScrollToTop />
+        
         <AnimatePresence mode="wait">
           <Routes>
             <Route path="/" element={<Home />} />
